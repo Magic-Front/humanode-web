268c4c748ada37fc4530bd1624a174fac453fd22
Uhmopt <qethum@gmail.com>
2021-12-23 20:14:41 +1100

    [New] about background

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -4,7 +4,7 @@ import AboutComponent from "./AboutComponent";
 
 export default function About() {
   return (
-    <Layout background="head-eye" maxWidth="xl" showHome>
+    <Layout background="head-eye rect-03" maxWidth="xl" showHome>
       <AboutComponent />
     </Layout>
   );
